418e80f577a32827503af655491c2fbfb38cab51
Pieter van Tienhoven <pieter@pieter.lol>
2026-05-30 23:49:08 +0200

    add linting, formatting, and release tooling

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
+
+export default [
+    js.configs.recommended,
+    prettier,
+    {
+        languageOptions: {
+            globals: globals.node,
+        },
+        rules: {
+            "no-unused-vars": "warn",
+            "no-console": "warn",
+        },
+    },
+];
